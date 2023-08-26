82d349719bcad6823c7ec9f5b5ee8a17ae0237c6
Jmytwenty8 <jmy2898@gmail.com>
2023-08-26 16:11:09 +0530

    Add Templates for different commands

--- src/commands.js
+++ src/commands.js
@@ -11,9 +11,61 @@ yargs(hideBin(process.argv))
         describe: "Content of note to create",
       });
     },
-    (argv) => {
+    async (argv) => {
       console.log(argv.note);
     }
   )
+  .command(
+    "all",
+    "get all notes",
+    () => {},
+    async (argv) => {}
+  )
+  .command(
+    "find <filter>",
+    "get matching notes",
+    (yargs) => {
+      return yargs.positional("filter", {
+        describe:
+          "The search term to filter notes by, will be applied to note.content",
+        type: "string",
+      });
+    },
+    async (argv) => {}
+  )
+  .command(
+    "remove <id>",
+    "remove a note by id",
+    (yargs) => {
+      return yargs.positional("id", {
+        type: "number",
+        description: "The id of the note you want to remove",
+      });
+    },
+    async (argv) => {}
+  )
+  .command(
+    "web [port]",
+    "launch website to see notes",
+    (yargs) => {
+      return yargs.positional("port", {
+        describe: "port to bind on",
+        default: 5000,
+        type: "number",
+      });
+    },
+    async (argv) => {}
+  )
+  .command(
+    "clean",
+    "remove all notes",
+    () => {},
+    async (argv) => {}
+  )
+  .options("tags", {
+    alias: "t",
+    type: "string",
+    describe: "Tags to add to note",
+  })
   .demandCommand(1)
   .parse();
